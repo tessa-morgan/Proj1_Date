801868f601515a09d29cf0d1d49fdd10bee1761c
Tessa Morgan <tessa.morgan870@gmail.com>
2025-03-03 00:34:08 -0600

    CPU Usage

--- client.c
+++ client.c
@@ -18,9 +18,13 @@ long get_response()
     printf("                1. Date\n");
     printf("                2. Time\n");
     printf("                3. Both\n");
-    printf("                4. Quit\n");
+    printf("                4. CPU Usage\n");
+    printf("                5. Memory Usage\n");
+    printf("                6. Process Count\n");
+    printf("                7. Load Procs per minute\n");
+    printf("                8. Quit\n");
     printf("-------------------------------------------\n");
-    printf("               Choice (1-4):");
+    printf("               Choice (1-8):");
     scanf("%ld",&choice);
     printf("===========================================\n");
     return(choice);
@@ -28,12 +32,12 @@ long get_response()
 
 main(int argc, char **argv)
 {
-    CLIENT  *cl;        /* RPC handle */
+    CLIENT  *cl;            /* RPC handle */
     char    *server;
-    char    **sresult;  /* return value from date_1()      */
-    char    s[MAX_LEN];    /* character array to hold output */
-    long    response;  /* user response                           */
-    long    *lresult;    /* pointer to user response          */
+    char    **sresult;      /* return value from date_1()      */
+    char    s[MAX_LEN];     /* character array to hold output */
+    long    response;       /* user response                           */
+    long    *lresult;       /* pointer to user response          */
 
     if (argc != 2) {
         fprintf(stderr, "usage: %s hostname\n", argv[0]);
